011159d7b35d07fb9e116a4879a7c1069d1ab7ee
12116681 <arthurcoutinho@gmail.com>
2023-02-24 20:26:18 -0300

    feat: cria a classe conta

---
+++ src/br/newtonpaiva/dominio/Conta.java
@@ -0,0 +1,23 @@
+package br.newtonpaiva.dominio;
+
+public class Conta {
+    private Integer numero;
+    private Double saldo;
+
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
