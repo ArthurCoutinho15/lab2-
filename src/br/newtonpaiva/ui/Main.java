4c58f03c67df822f40b99604ffe00fb7bfe95e59
12116681 <arthurcoutinho@gmail.com>
2023-03-03 21:22:08 -0300

    feat: implementa construtores, equals e hashcode do código

---
+++ src/br/newtonpaiva/ui/Main.java
@@ -0,0 +1,28 @@
+package br.newtonpaiva.ui;
+
+import br.newtonpaiva.dominio.Conta;
+
+public class Main {
+    public static void main(String[] args) {
+        Conta c = new Conta(10, 100.0);
+        c.setNumero(10);
+        c.setSaldo(100.0);
+
+        c.depositar(50.0);
+
+        Conta c2 = new Conta();
+        c2.setNumero(90);
+        c2.setSaldo(500.0);
+
+
+        Conta c3 = new Conta();
+
+        if(c.equals(c2)){
+            System.out.println("Mesma conta");
+        }
+        else{
+            System.out.println("Contas diferentes");
+        }
+        System.out.println(c.getSaldo());
+    }
+}
